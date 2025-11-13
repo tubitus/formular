82fa08235692f077de87e0303864cdad26a493c1
Jakub Vlček <Jakub.Vlcek@student.souepl.cz>
2025-11-13 13:42:23 +0100

    firts try

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/styles/style.css">
+    <script src="/script/script.js" defer></script>
+</head>
+<body>
+    <header>
+        <h1>Zde je Header</h1>
+    </header>
+    <main>
+        <dev class="container">
+            <section class="nadpis">
+                <img src="/imgs/apexlogo.png" alt="">
+                <h1>Create an account</h1>
+            </section>
+            <section class="form">
+                <form action="">
+                    <p>Username</p>
+                    <input type="text" required>
+                    <p>Email</p>
+                    <input type="email" required>
+                    <p>Password</p>
+                    <input type="password" required>
+                    <div class="terms">
+                        <input type="checkbox" name="terms" id="terms" > 
+                        <label for="terms">I agree to the <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a></label>
+                    </div>
+                    <button type="submit">Sign Up</button>
+                </form>
+            <section class="sign_in">
+                <p>Already have an account? </p>
+                <a href="#">Sign In</a>
+            </section>
+        </dev>
+    </main>
+    <footer>
+        <h1>Zde je Footer</h1>
+    </footer>
+</body>
+</html>
